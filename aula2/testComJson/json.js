fec5ae5209f52ad15a8fed9ed4ccb2f443e6555d
Kevin <kevinrezende55@outlook.com>
2026-04-28 21:10:26 -0300

    teoria | estudo Proprio

---
+++ aula2/testComJson/json.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("meuFormulario");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); //previne que o site nao der reload ao clicar no butao
+
+    console.log("Formulário capturado!");
+
+
+    console.log(nomeCompleto)
+
+
+    // depois você vai pegar os valores aqui
+});
